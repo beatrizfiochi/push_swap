22ec5de62e17e3c669ef425004f40d2d960e52f8
Beatriz Fiochi Soares Da Silva <bfiochi-@c2r6s7.42porto.com>
2025-03-27 16:56:02 +0000

    adding a new function to fix norms

--- sort.c
+++ sort.c
@@ -6,18 +6,45 @@
 /*   By: bfiochi- <bfiochi-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:06:33 by bfiochi-          #+#    #+#             */
-/*   Updated: 2025/03/26 17:54:09 by bfiochi-         ###   ########.fr       */
+/*   Updated: 2025/03/27 16:54:04 by bfiochi-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+static void	last_op(t_list **stack)
+{
+	int		median_of_lst;
+	t_list	*min_node;
+
+	put_index(stack);
+	min_node = find_min(stack);
+	if (((t_node *)(min_node->content))->index != 0)
+	{
+		median_of_lst = median(stack);
+		if (((t_node *)(min_node->content))->index >= median_of_lst)
+		{
+			while (((t_node *)(min_node->content))->index != 0)
+			{
+				rra(stack);
+				put_index(stack);
+			}
+		}
+		else
+		{
+			while (((t_node *)(min_node->content))->index != 0)
+			{
+				ra(stack);
+				put_index(stack);
+			}
+		}
+	}
+}
+
 void	sort(t_list **stack_a, t_list **stack_b)
 {
 	int		lst_size_a;
 	int		lst_size_b;
-	int		median_of_lst;
-	t_list	*min_node;
 
 	while (ft_lstsize(*stack_a) > 3 && ft_lstsize(*stack_b) < 2)
 		pb(stack_a, stack_b);
@@ -34,27 +61,6 @@ void	sort(t_list **stack_a, t_list **stack_b)
 		execute_final_steps(stack_b, stack_a);
 		lst_size_b = ft_lstsize(*stack_b);
 	}
-	put_index(stack_a);
-	min_node = find_min(stack_a);
-	if (((t_node *)(min_node->content))->index != 0)
-	{
-		median_of_lst = median(stack_a);
-		if (((t_node *)(min_node->content))->index >= median_of_lst)
-		{
-			while (((t_node *)(min_node->content))->index != 0)
-			{
-				rra(stack_a);
-				put_index(stack_a);
-			}
-		}
-		else
-		{
-			while (((t_node *)(min_node->content))->index != 0)
-			{
-				ra(stack_a);
-				put_index(stack_a);
-			}
-		}
-	}
+	last_op(stack_a);
 	return ;
 }
